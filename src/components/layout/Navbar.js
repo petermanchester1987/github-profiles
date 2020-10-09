5b9da0084e71883f4d459f271f136288793ae28e
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-10-09 12:26:23 +0100

    added nav bar, userItem and users components

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,10 +1,23 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 export class Navbar extends Component {
+  static defaultProps = {
+    title: "Developer Profiles",
+    icon: "fab fa-github",
+  };
+
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+  };
+
   render() {
     return (
       <nav className="navbar bg-primary">
-        <h1>Navbar</h1>
+        <h1>
+          <i className={this.props.icon}></i> {this.props.title}
+        </h1>
       </nav>
     );
   }
