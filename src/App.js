4d075ec34cfab99f7b457932c5b3ff6cb88174d4
peter <64981120+petermanchester1987@users.noreply.github.com>
2020-10-09 13:11:18 +0100

    added axios call to github

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from "react";
+import axios from "axios";
 import "./App.css";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 
 class App extends Component {
+  state = {
+    users: [],
+    loading: false,
+  };
+
+  // USING .then syntax
+  // componentDidMount() {
+  //   axios
+  //     .get("https://api.github.com/users")
+  //     .then((res) => console.log(res.data));
+  // }
+  async componentDidMount() {
+    this.setState({ loading: true });
+
+    const res = await axios.get("https://api.github.com/users");
+
+    this.setState({ users: res.data, loading: false });
+  }
+
   render() {
     return (
       <div className="App">
         <Navbar />
         <div className="container">
-          <Users />
+          <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </div>
     );
